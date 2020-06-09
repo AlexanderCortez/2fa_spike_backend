a552823e43210eaad34bdfd860284d237f3063df
Alexander Cortez <joelalexander10@outlook.com>
2020-06-08 22:49:43 -0600

    :rocket: 2fa manually

--- client/precache-manifest.6ed758334f8da1138e95b1943d728520.js
+++ client/precache-manifest.6ed758334f8da1138e95b1943d728520.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "674bff66b4baba45516231a29718a6e8",
+    "revision": "fe4abc6bb8510d589b000025c75b661e",
     "url": "/index.html"
   },
   {
-    "revision": "905885d9a32571173a42",
+    "revision": "77db4783ff8d761d95dc",
     "url": "/static/css/2.9832adca.chunk.css"
   },
   {
-    "revision": "409ac5c74847e6dc5a75",
+    "revision": "7edb419deec3dd16ab4c",
     "url": "/static/css/main.e247f211.chunk.css"
   },
   {
-    "revision": "905885d9a32571173a42",
-    "url": "/static/js/2.e5f3f227.chunk.js"
+    "revision": "77db4783ff8d761d95dc",
+    "url": "/static/js/2.8af69cca.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.e5f3f227.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8af69cca.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "409ac5c74847e6dc5a75",
-    "url": "/static/js/main.fc998e8b.chunk.js"
+    "revision": "7edb419deec3dd16ab4c",
+    "url": "/static/js/main.6ec31eb4.chunk.js"
   },
   {
     "revision": "de0499528e71a44eee06",
